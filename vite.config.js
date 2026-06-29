c0e3ad97f1d856672d068e9569024fb0d9a4d455
chinagliaalberto96-spec <chinaglia.alberto96@gmail.com>
2026-06-29 19:03:57 +0200

    Gap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  esbuild: {
+    jsx: "automatic"
+  },
+  build: {
+    reportCompressedSize: false,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"]
+        }
+      }
+    }
+  }
 });
